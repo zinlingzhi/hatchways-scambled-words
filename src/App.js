8e59348df993a11bcf70e6663e2f9fdf7db00b70
zinlingzhi <zinlingzhi@gmail.com>
2022-04-17 08:00:45 +0800

    Upgrade key board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import useScramble from './hooks/useScramble';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
-  const [sentence, getSentence] = useState('')
+  const [fetchedSentence, setFetchedSentence] = useState('');
+  const [score, setScore] = useState(0);
   const readSentence = () => {
     axios.get('https://api.hatchways.io/assessment/sentences/1')
       .then((response) => {
         // const sentences = 
-        getSentence(response.data.data.sentence)
+        setFetchedSentence(response.data.data.sentence)
       })
   }
 
   useEffect(() => {
     readSentence()
   },[])
+
+  const scrambeledSentence = useScramble(fetchedSentence);
+  const words = fetchedSentence.split(" ");
+  var split_words = [];
+  words.map((word, index) => {
+    const word_array = word.split("")
+    split_words.push(word_array);
+  })
   return (
     <div className="App">
-      <p>{sentence}</p>
+      <div className="main">
+          <p className="scrambled_sentence">{scrambeledSentence}</p>
+          <p className="explain_text">Guess the sentence! Start typing. </p>
+          <p className="explain_text"> The yellow blocks are meant for spaces</p>
+          <p className="score_text">Score: {score}</p>
+          <div className="keyboard">
+            {
+              split_words.map((words, index) => (
+                <div key={`word-${index}`} className='word'>
+                  {words.map((char, index1) =>(
+                    <CharDiv className='char' count={index !== split_words.length-1 ? words.length+1 : words.length}>
+                      <input className="char_input" maxLength={1}/>
+                    </CharDiv>
+                  ))}
+                  {index !== split_words.length-1 && (
+                    <CharDiv className='char' count={words.length+1}>
+                      <input className="char_input space" maxLength={1} autoFocus={true}></input>
+                    </CharDiv>
+                  )}
+                </div>
+              ))
+            }
+          </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const CharDiv = styled.div`
+  width: ${props => 100 / props.count}%;
+  background: white;
+  padding: 10px;
+
+  .char_input{
+    outline: 0;
+    border: 0;
+    background-color: #e1e1e1;
+    height: 50px;
+    text-align: center;
+    font-size: 16px;
+    &.space{
+      background-color: #ffb74d;
+    }
+  }
+`
--- src/hooks/useScramble.js
+++ src/hooks/useScramble.js
@@ -1,9 +1,31 @@
-import {useState, useEffect} from 'react';
 
 function useScramble(sentence) {
-  
+  // Seperate words by space
+  const words = sentence.split(' ');
+  let res = '';
+  words.map((word, index) => {
+    res += shuffle(word)
+    if (index !== words.length-1)
+      res += " "
+  })
+  return res
 }
 
+function shuffle(word) {
+  var characters = word.split("")
+  var length = characters.length;
+  if (length > 2){
+    for(var i = length - 2; i > 1; i--){
+      var j = Math.floor(Math.random() * (i+1));
+      j = j === 0 || j === length-1 ? 1 : j;
+      var temp = characters[i];
+      characters[i] = characters[j];
+      characters[j] = temp;
+    }
+  }
+
+  return characters.join("");
+}
 export default useScramble;
 
 
